246d5c70ff714a2dfb8d128f5b958968f4543d62
Nmikh <nmikhaluk@gmail.com>
2019-01-30 16:08:03 +0200

    svm kernel params entety

---
+++ src/main/java/com/models/entity/specialist/SvmKernelParametrsEntity.java
@@ -0,0 +1,47 @@
+package com.models.entity.specialist;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "SVM_KERNEL_PARAMETRS")
+public class SvmKernelParametrsEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private long id;
+
+    @Basic
+    @Column(name = "NAME")
+    private String name;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        SvmKernelParametrsEntity that = (SvmKernelParametrsEntity) o;
+        return id == that.id &&
+                Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+}
